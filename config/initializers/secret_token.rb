830f70f2b1c9a2b1884cb6ed41c61a8028c0c69e
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-10-06 23:36:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cs657Phonebook::Application.config.secret_token = '071b20d98caf50cab7f9cf8f4e7fe04abd9f8d0bf4f2364632ca493431625a6f49266a2d1c961aafcdf2680cebef388a6f7e73f1af919eb7a3fa1941cfe9f9ee'
