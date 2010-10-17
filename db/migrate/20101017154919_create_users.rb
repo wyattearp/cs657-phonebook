424c2f4ead63a080891ffd2ab2e87de08ddd8ce7
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-10-17 13:11:18 -0400

    adding in basic user model

---
+++ db/migrate/20101017154919_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :password
+      t.string :email_address
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
