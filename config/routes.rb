f79b677d0fa749951500f72cbe177579324e382a
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-10-24 13:29:20 -0400

    adding in authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Cs657Phonebook::Application.routes.draw do
+  resources :users do
+    member do
+      put :suspend
+      put :unsuspend
+      delete :purge
+    end
+  end
+
+  resource :session, :only => [:new, :create, :destroy]
+
+  match 'signup' => 'users#new', :as => :signup
+
+  match 'register' => 'users#create', :as => :register
+
+  match 'login' => 'sessions#new', :as => :login
+
+  match 'logout' => 'sessions#destroy', :as => :logout
+
+  match '/activate/:activation_code' => 'users#activate', :as => :activate, :activation_code => nil
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,10 +69,11 @@ Cs657Phonebook::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  # root :to => "users#show"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
