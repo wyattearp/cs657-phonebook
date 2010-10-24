456786fb06fd73f005f8d6c8b833b37c4cb0d166
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-10-24 14:27:35 -0400

    adding in default route to application/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -73,7 +73,7 @@ Cs657Phonebook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "application#index"
   # root :to => "users#show"
 
   # See how all your routes lay out with "rake routes"
