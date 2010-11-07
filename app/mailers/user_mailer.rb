bfe2c6c286569a6b370bc86ea3e86df90f9dd1da
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-11-07 13:51:57 -0500

    adding in first and last names

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-       @url  = "http://YOURSITE/activate/#{user.activation_code}"
+       @url  = "http://127.0.0.1:3000/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @url  = "http://YOURSITE/"
+    @url  = "http://127.0.0.1:3000/"
   end
   
   protected
 
   def setup_email(user)
     @recipients  = "#{user.email}"
-    @from        = "ADMINEMAIL"
-    @subject     = "[YOURSITE] "
+    @from        = "admin@netpages.com"
+    @subject     = "[NET PAGES] "
     @sent_on     = Time.now
     @user = user
   end
