6722ccbb2dcd780d4b9e6bb04513d8d992e730a9
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-11-07 18:35:21 -0500

    emails config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,9 @@ Cs657Phonebook::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
 
