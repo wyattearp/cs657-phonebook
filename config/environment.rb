99f3633cbe982a753ceb6622ba661ab6b757943c
CAL <cal@somewhere.com>
2010-11-07 18:30:43 -0500

    Emailing phonebooks as attachments is done. Also fixed a greedy find-and-replace of Wyatt's.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# We need FasterCSV for exporting to CSV files
+require 'fastercsv'
+
 # Initialize the rails application
 Cs657Phonebook::Application.initialize!
