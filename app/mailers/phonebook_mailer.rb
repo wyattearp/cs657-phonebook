4370290870e4062e91ca8b203dac71693ed064b2
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-11-07 22:54:48 -0500

    fixing up to send attachement with only 1 set of quotes

--- app/mailers/phonebook_mailer.rb
+++ app/mailers/phonebook_mailer.rb
@@ -6,24 +6,25 @@ class PhonebookMailer < ActionMailer::Base
     @message = email[:message]
 
     # store the phonebook in Comma Seperated Values
-    csv_addresses = FasterCSV.generate do |csv|
+    #TODO: doesn't do error correction on filenames
+    filename = @phonebook.name + ".csv"
+    FasterCSV.open(filename, "w", { :force_quotes => true }) do |csv|
       # create the header
       csv << ["name", "phone", "address1", "address2", "city", "state", "zip"]
 
       # output a row of phonebook data
       @phonebook.entries.each do |entry|
-        csv << ["\"#{entry.name}\"", "\"#{entry.phone}\"", "\"#{entry.address1}\"", "\"#{entry.address2}\"", "\"#{entry.city}\"", "\"#{entry.state}\"", "\"#{entry.zip}\""]
+        csv << ["#{entry.name.to_s}", "#{entry.phone}", "#{entry.address1}", "#{entry.address2}", "#{entry.city}", "#{entry.state}", "#{entry.zip}"]
       end
     end
 
     # Create a CSV file named after the phonebook
-    #TODO: doesn't do error correction on filenames
-    filename = @phonebook.name + ".csv"
-    csv_file = File.new(filename, "w")
-    csv_file.puts(csv_addresses)
+    csv_file = File.open(filename, "r")
 
     # Create and send the email using the to and subject fields and attaching the requested file
-    mail(:from => @user.email, :to => email[:to], :subject => email[:subject], :attachment => filename)
+
+    attachments[filename] = csv_file.read
+    mail(:from => @user.email, :to => email[:to], :subject => email[:subject])
 
     # delete the file so we don't leave stale files and personal data on the computer
     File.delete(filename)
